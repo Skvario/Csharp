e0a82f83af60bdc8d1da30fe88045292361969ca
Dmitry <skvar1989@gmail.com>
2024-01-10 17:07:08 +0300

    Sem1T1 add

--- Csharp/seminar2/task1/Program.cs
+++ Csharp/seminar2/task1/Program.cs
@@ -2,14 +2,17 @@
 // а на выходе удаляет вторую цифру этого числа
 // a=256=>26
 // a = 891=>81
-
-int num = 23;
-if(num > 99 && num <= 999){
-    int num1 = num / 100;
-    int num3 = num % 10;
-    int result = num1 * 10 + num3;
+Console.WriteLine("enter your number: ");
+// string? num = Console.Readline();
+int num1 = Convert.ToInt32(Console.ReadLine());
+if (num1 > 99 && num1 <= 999)
+{
+    int num2 = num1 / 100;
+    int num3 = num1 % 10;
+    int result = num2 * 10 + num3;
     Console.Write(result);
 }
-else{
+else
+{
     Console.Write("your numb is not 3 digit , motherfucker!");
 }
