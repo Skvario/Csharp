ddecc0038d503682c0a4b62f315cf5391d1d69a2
Dmitry <skvar1989@gmail.com>
2024-01-14 13:34:09 +0300

    Sem2T3

---
+++ Csharp/seminar2/task3/Program.cs
@@ -0,0 +1,15 @@
+﻿//  программа принимает на вход два числа,
+// и выводит является ли второе чило кратным 
+// первому, если нет , то выводит остаток от деления
+
+int first = 3;
+int second = 19;
+int third = second % first ;
+
+if(third == 0){
+    Console.WriteLine("yes");
+}
+else{
+    Console.WriteLine($"no, {third}"); 
+    Console.WriteLine($"no, {second % first}");  
+}
