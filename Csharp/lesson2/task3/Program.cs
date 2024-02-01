f2ae01bff960822b2f57d135685e1c60660aab14
Dmitry <skvar1989@gmail.com>
2024-02-02 00:20:09 +0300

    Lesson2T3

---
+++ Csharp/lesson2/task3/Program.cs
@@ -0,0 +1,13 @@
+﻿//int n = 10;
+int[] array = { 2, 5, 4, 7, 6, 23, 78, 53, 22, 23 };
+int i = 0;
+while(i < array.Length)
+{
+    if(array[i] % 2 == 0)
+    {
+        Console.Write($"{array[i]} ");    
+    }
+    i = i + 1;
+}
+
+
