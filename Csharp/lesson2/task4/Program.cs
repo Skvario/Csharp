02191ea74a0007e18509b33939b578ef85e40763
Dmitry <skvar1989@gmail.com>
2024-02-02 01:14:49 +0300

    Lesson2T4

---
+++ Csharp/lesson2/task4/Program.cs
@@ -0,0 +1,36 @@
+﻿int n = 5;
+int[] array = { 3, 4, 99, 6, 3 };
+int i = 0;
+int max = array[0];
+
+while(i < n)
+{
+    if (array[i] > max)
+    {
+    max = array[i];
+    }
+    i = i + 1;
+} 
+Console.Write($"{max} ");
+
+max = array[0];
+for(int j = 0; j < n; j++)
+{
+    if(array[j] > max)
+    {
+        max = array[j];
+    }
+} 
+
+Console.Write($"{max} ");
+
+max = array[0];
+foreach(int e in array)
+{
+    if(e > max)
+    {
+        max = e;
+    }
+}
+
+Console.Write($"{max} ");
