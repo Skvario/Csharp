b76dd2ea420bb6624f5a2bcbef4a7e8c39d95bc3
Dmitry <skvar1989@gmail.com>
2024-10-30 15:56:33 +0300

    forget about it

--- Csharp/tryTOuse/forgot/Program.cs
+++ Csharp/tryTOuse/forgot/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+// int value = 100;
+// Console.WriteLine(value);
+double a = 5.55;
+char b = '!';
+bool c = true;
+Console.Write(a);
+Console.Write(b);
+Console.Write(c);
